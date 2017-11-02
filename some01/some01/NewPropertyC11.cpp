14aab94992fe45db2b08f93767cd0d14c7623f79
heluicare <heludoit@126.com>
2017-11-02 22:40:45 +0800

    Update NewPropertyC11.cpp

--- some01/some01/NewPropertyC11.cpp
+++ some01/some01/NewPropertyC11.cpp
@@ -8,7 +8,30 @@ NewPropertyC11::~NewPropertyC11()
 {
 }
 
-void NewPropertyC11::funPointTest()
+int funcTest01(int a, int b)
 {
-	
+	cout << a << "," << b << endl;
+	return 0;
+}
+
+void NewPropertyC11::funcPointTest()
+{
+	// 声明一个函数指针，指针funcPt指向一个返回值为int且两个int参数的函数
+	int (*funcPt)(int, int);
+	funcPt = funcTest01;
+	//funcPt = &funcTest01;
+	//funcPt = *funcTest01;
+	funcPt(2, 3);
+
+	typedef int(*FuncPt)(int, int);
+	FuncPt funcpt01 = funcTest01;
+	//FuncPt funcpt01 = &funcTest01;
+	//FuncPt funcpt01 = *funcTest01;
+	funcpt01(2, 3);
+
+}
+
+void NewPropertyC11::funcTempeletTest()
+{
+
 }
