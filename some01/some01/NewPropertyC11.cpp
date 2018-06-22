566903a0ae62a23f1ff052d073c10f46aa2a0149
heluicare <heludoit@126.com>
2018-06-22 17:26:36 +0800

    Update NewPropertyC11.cpp

--- some01/some01/NewPropertyC11.cpp
+++ some01/some01/NewPropertyC11.cpp
@@ -1,74 +1,72 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
 #include "stdafx.h"
+#include <functional>
+#include <iostream>
 
-NewPropertyC11::NewPropertyC11()
+struct Foo {
+	Foo(int num) : num_(num) {}
+	void print_add(int i) const { std::cout << num_ + i << '\n'; }
+	int num_;
+};
+
+void print_num(int i)
 {
+	std::cout << i << '\n';
 }
 
-NewPropertyC11::~NewPropertyC11()
-{
-}
+struct PrintNum {
+	void operator()(int i) const
+	{
+		std::cout << i << '\n';
+	}
+};
 
-int funcTest01(int a, int b)
+
+int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << a << "," << b << endl;
+	// 存储 自由函数
+	std::function<void(int)> f_display = print_num;
+	f_display(-9);
+
+	// 存储 lambda
+	std::function<void()> f_display_42 = []() { print_num(42); };
+	f_display_42();
+
+	// 存储 std::bind调用的结果
+	// std::bind的特殊作用https://www.cnblogs.com/ljygoodgoodstudydaydayup/p/5813247.html
+	std::function<void()> f_display_31337 = std::bind(print_num, 31337);
+	f_display_31337();
+
+	//// 存储 成员函数的调用
+	std::function<void(const Foo, int)> f_add_display = &Foo::print_add;
+	const Foo foo(314159);
+	f_add_display(foo, 1);
+	f_add_display(314159, 1);
+
+	// 存储 数据成员访问器的调用
+	std::function<int(Foo const)> f_num = &Foo::num_;
+	std::cout << "num_: " << f_num(foo) << '\n';
+
+	// 存储 成员函数和调用成员函数对象的调用
+	using std::placeholders::_1;
+	std::function<void(int)> f_add_display2 = std::bind(&Foo::print_add, foo, _1);
+	f_add_display2(2);
+
+	// 存储 成员函数和调用成员函数对象不使用占位符的调用
+	std::function<void()> f_add_display2_1 = std::bind(&Foo::print_add, foo, 1);
+	f_add_display2_1();
+
+	// 存储 成员函数和调用成员函数对象指针的调用
+	std::function<void(int)> f_add_display3 = std::bind(&Foo::print_add, &foo, _1);
+	f_add_display3(3);
+
+	// 存储 函数对象的调用
+	std::function<void(int)> f_display_obj = PrintNum();
+	f_display_obj(18);
+
+	system("pause");
 	return 0;
 }
 
-void NewPropertyC11::funcPointTest()
-{
-	funcPt0 = funcTest01;
-	//funcPt = &funcTest01;
-	//funcPt = *funcTest01;
-	funcPt0(2, 3);
-
-	FuncPt1 funcpt01 = funcTest01;
-	//FuncPt funcpt01 = &funcTest01;
-	//FuncPt funcpt01 = *funcTest01;
-	funcpt01(2, 3);
-
-}
-
-
-template <typename T>
-void funcTest02(T& a, T& b)
-{
-	cout << a << "," << b << endl;
-}
-
-void NewPropertyC11::funcTemplateTest()
-{
-	int a0 = 1;
-	int a1 = 2;
-	funcTest02(a0, a1);
-	float b0 = 1.0f;
-	float b1 = 2.0f;
-	funcTest02(b0, b1);
-	//funcTest02(b0, b1);
-	int* c0 = &a0;
-	int* c1 = &a1;
-	funcTest02(c0, c1);
-	int& d0 = a0;
-	int& d1 = a1;
-	funcTest02(d0, d1);
-	
-	funcTest02(funcTest01, funcTest01);
-
-}
-
-void NewPropertyC11::classTemplateTest()
-{
-	
-}
-
-void NewPropertyC11::funcClassTemplateTest()
-{
-	// 初始化一个类模板
-	std::function<int(int, int)> func01 = funcTest01;
-	// 
-	std::function<void> func02 = funcPointTest;
-	// 函数模板
-	std::function<int(int, int)> func03 = std::bind(&funcTest01, placeholders::_1, placeholders::_2);
-	// 
-	std::function<int(int, int)> func03 = std::bind(&funcPointTest, placeholders::_1, placeholders::_2);
-
-}
